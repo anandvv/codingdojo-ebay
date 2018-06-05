f749b231bb2faca2340c7060dcabe12293c68077
Anand V V <anand.vijay@gmail.com>
2018-06-05 15:45:47 -0700

    EJS Cat Data Exercise

---
+++ Anand/04_Express/ejs_cat_data/app.js
@@ -0,0 +1,97 @@
+// Load the express module and store it in the variable express (Where do you think this comes from?)
+var express = require("express");
+console.log("Let's find out what express is", express);
+
+// invoke express and store the result in the variable app
+var app = express();
+console.log("Let's find out what app is", app);
+
+// this is the line that tells our server to use the "/static" folder for static content
+app.use(express.static(__dirname + "/static"));
+// two underscores before dirname
+// try printing out __dirname using console.log to see what it is and why we use it
+
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views'); 
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+
+// use app's get method and pass it the base route '/' and a callback
+app.get('/', function(request, response) {
+    // just for fun, take a look at the request and response objects
+   console.log("The request object", request);
+   console.log("The response object", response);
+   // use the response object's .send() method to respond with an h1
+   response.sendFile(__dirname + "/index.html");
+})
+
+app.get('/cars', function(request, response) {
+    // just for fun, take a look at the request and response objects
+   console.log("The request object", request);
+   console.log("The response object", response);
+   // use the response object's .send() method to respond with an h1
+   response.render("cars");
+})
+
+app.get('/cats', function(request, response) {
+    // just for fun, take a look at the request and response objects
+   console.log("The request object", request);
+   console.log("The response object", response);
+   // use the response object's .send() method to respond with an h1
+   response.render("cats");
+})
+
+app.get('/cats/cuddles', function(request, response) {
+    // just for fun, take a look at the request and response objects
+   console.log("The request object", request);
+   console.log("The response object", response);
+
+   var catData = 
+        {
+            name: "Cuddles", 
+            url: "/images/cuddles.jpg",
+            favorite_food: "Fish",
+            sleeping_spots: ['under the bed', 'in the sunbeam']
+        };
+
+   // use the response object's .send() method to respond with an h1
+   response.render("catdetails", {catInfo: catData});
+})
+
+app.get('/cats/tiggy', function(request, response) {
+    // just for fun, take a look at the request and response objects
+   console.log("The request object", request);
+   console.log("The response object", response);
+
+   var catData = 
+        {
+            name: "Tiggy", 
+            url: "/images/tiggy.jpg",
+            favorite_food: "Salmon",
+            sleeping_spots: ['on the couch', 'inside comforter']
+        };
+
+   // use the response object's .send() method to respond with an h1
+   response.render("catdetails", {catInfo: catData});
+})
+
+app.get('/cats/tiggy', function(request, response) {
+    // just for fun, take a look at the request and response objects
+   console.log("The request object", request);
+   console.log("The response object", response);
+   // use the response object's .send() method to respond with an h1
+   response.render("catdetails");
+})
+
+app.get('/cars/new', function(request, response) {
+    // just for fun, take a look at the request and response objects
+   console.log("The request object", request);
+   console.log("The response object", response);
+   // use the response object's .send() method to respond with an h1
+   response.render("newcar");
+})
+
+// tell the express app to listen on port 8000, always put this at the end of your server.js file
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+})
