507d09ab188b03f7aba46459abb307253c7ebbd1
spachineela <spachineela@ebay.com>
2018-06-08 14:29:25 -0700

    adding player ejs and updating players json file
    Signed-off-by: spachineela <spachineela@ebay.com>

--- Leon/Final_Project/routes/routes.js
+++ Leon/Final_Project/routes/routes.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router()
+var router = express.Router();
 
 
 var mongoose = require('mongoose');
@@ -30,10 +30,10 @@ router.get('/', function(req, res) {
 });
 
 router.get('/player/:id', function(req, res) {
-  // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
+  // TODO change api to find by id
   Players.find({}, (err, playersData) => {
-    console.log(playersData);
-    res.render('player', {players: playersData});
+    console.log(playersData[1]);
+    res.render('player', {player: playersData[2]});
   });
 });
 
