ecd5ce4c3fb0104b4200936d8eb736c17d118ad5
Anand V V <anand.vijay@gmail.com>
2018-06-08 15:47:33 -0700

    Results page and update to Server1.js for results route

--- Leon/Final_Project/server1.js
+++ Leon/Final_Project/server1.js
@@ -2,6 +2,9 @@
 var express = require('express');
 // Create an Express App
 var app = express();
+
+var axios = require('axios');
+
 // Require body-parser (to receive post data from clients)
 var bodyParser = require('body-parser');
 
@@ -28,6 +31,7 @@ var db = mongoose.connection;
 
 // Integrate body-parser with our App
 app.use(bodyParser.urlencoded({ extended: true }));
+
 // Require path
 var path = require('path');
 // Setting our Static Folder Directory
@@ -44,6 +48,48 @@ app.get('/', function(req, res) {
       console.log(docs);
       res.render('index', { messages: docs });
     });
+});
+
+app.get('/results', function(req, res) {
+  // Construct the request
+  // Replace MyAppID with your Production AppID
+  var fs = require('fs'),
+    xml2js = require('xml2js');
+
+  var parser = new xml2js.Parser();
+  fs.readFile(__dirname + '/ebay_findItemsByCategory.xml', function (err, data) {
+      //parser.parseString(data, function (err, result) {
+          //console.dir(result);
+          //console.log('Done');
+          //console.log(JSON.parse(stringifiedResult));
+          // var axiosConfig = {
+          //   headers: {
+          //     'Content-Type': 'text/xml',
+          //     'X-EBAY-SOA-SECURITY-APPNAME': 'AnandVij-ProSport-PRD-55d80d3bd-bd415843',
+          //     'X-EBAY-SOA-OPERATION-NAME' : 'findItemsByCategory'
+          //   }
+          // };
+          axios.get(
+                      "https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.13.0&RESPONSE-DATA-FORMAT=JSON&categoryId=115280&GLOBAL-ID=EBAY-US&siteid=0&SECURITY-APPNAME=PakHeiIe-codingdo-PRD-0b058513b-a39b18f0"
+                      // data, axiosConfig
+                      // ).then(function(response){
+                      //     parser.parseString(response.data, function(err, data){
+                      //       for(index in data.searchResult){
+                      //         console.log(data.searchResult[index].item.itemId);
+                      //       }
+                      //     });
+                      // }
+                    ).then(function(response){
+                      res.send(response.data);
+                          // parser.parseString(response.data, function(err, data){
+                          //   for(index in data.searchResult){
+                          //     console.log(data.searchResult[index].item.itemId);
+                          //   }
+                          })
+                      .catch((err) => {
+                        console.log(err);
+                      });
+      });
 });
 
 app.listen(3000, () => {
